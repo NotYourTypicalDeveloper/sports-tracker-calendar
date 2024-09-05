1a88bba6a589f6a9f6ab155d2211a77d95754f16
NotYourTypicalDeveloper <carolineho1983@gmail.com>
2024-09-05 20:38:42 +0100

    Updated homepage with SVG and styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,41 @@
 import Calendar from "./Calendar";
 import CTASection from "./CTASection";
+import StopWatchSVG from "../public/assets/stopwatch.svg";
+import SneakersSVG from "../public/assets/sneakers.svg";
+import Image from "next/image";
 
 export default function Hero() {
   return (
     <div className="flex flex-col gap-8 py-4 md:py-10 sm:gap-10">
-      <h1 className="text-5xl text-center sm:text-6xl md:text-7xl ">
-        <span className="textGradient">Be fit and motivated.</span>
-        👟 Track your <span>daily, weekly</span>{" "}
-        <span className="textGradient"> physical activities!</span>
-      </h1>
-      <p className="text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]">
-        Log your sports activities{" "}
-        <span className="font-semibold">every day of every year.</span>
-      </p>
+      <div className="text-5xl text-center sm:text-6xl md:text-7xl ">
+        <h1 className="textGradient">Be fit and motivated.</h1>
+        <p className="text-balance">
+          Track your <span className="textGradient">daily, weekly</span>{" "}
+          <span> physical activities!</span>
+        </p>
+        <Image
+          className="ml-2 -rotate-6"
+          src={SneakersSVG}
+          alt="sneakers image"
+          width={70}
+          height={70}
+        />
+      </div>
+
+      <div className="flex mx-auto">
+        <p className="text-lg sm:text-xl md:text-2xl text-center w-full max-w-[600px]">
+          Log your achievements and goals with{" "}
+          <span className="font-semibold">SportsTracker.</span>
+        </p>{" "}
+        <Image
+          className="ml-2 rotate-12"
+          src={StopWatchSVG}
+          alt="stopwatch image"
+          width={60}
+          height={60}
+        />
+      </div>
+
       <CTASection />
       <Calendar />
     </div>
